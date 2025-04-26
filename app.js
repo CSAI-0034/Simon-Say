d9153f69f97cec2e3c59bd0d9c2b139b4ccabcfe
Anubhav Tripathi <2k22.csai.2211940@gmail.com>
2025-04-26 17:00:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,86 +1,83 @@
-const topRight=document.querySelector('.top-right');
-const bottomRight=document.querySelector('.bottom-right');
-const topLeft=document.querySelector('.top-left');
-const bottomLeft=document.querySelector('.bottom-left');
+const topRight = document.querySelector('.top-right');
+const bottomRight = document.querySelector('.bottom-right');
+const topLeft = document.querySelector('.top-left');
+const bottomLeft = document.querySelector('.bottom-left');
 
-const getRandomPanel = () =>{
-    const panels=[
-        topRight,
-        bottomRight,
-        topLeft,
-        bottomLeft
-    ]
+const panels = [topRight, bottomRight, topLeft, bottomLeft];
+
+const getRandomPanel = () => {
     return panels[parseInt(Math.random() * panels.length)];
 }
 
-let canClick=false;
-const sequence=[getRandomPanel()];
-let sequenceToGuess=[...sequence]
-
+let canClick = false;
+let count = 0;
+const sequence = [getRandomPanel()];
+let sequenceToGuess = [...sequence];
 
 const sounds = {
     btn: new Audio('click-button-166324.mp3'),
 };
 
-const flash = (panel)=> {
-    console.log(panel);
-    return new Promise((resolve,reject) => {
-        panel.classList.add('colo')
-        setTimeout(() =>{
+const flash = (panel) => {
+    return new Promise((resolve) => {
+        panel.classList.add('colo');
+        setTimeout(() => {
             panel.classList.remove('colo');
             setTimeout(() => {
                 resolve();
-            },250)
-        },1000) 
-    })
+            }, 250);
+        }, 1000);
+    });
 }
- 
-const playSound = (panel) => {
-    if (panel === topRight) {
-        sounds.btn.play();
-    } else if (panel === bottomRight) {
-        sounds.btn.play();
-    } else if (panel === topLeft) {
-        sounds.btn.play();
-    } else if (panel === bottomLeft) {
-        sounds.btn.play();
-    }
-};
 
-let count=0;
-const PanelClicked=(panelClicked) => {
-    // if(!canClick) return;
-    const expectedPanel=sequenceToGuess.shift();
-    if(expectedPanel===panelClicked){
-        count +=1;
-        playSound(panelClicked);
-        if(sequenceToGuess.length===0){
+const playSound = () => {
+    sounds.btn.play();
+}
+
+const resetGame = () => {
+    alert("Wrong! Your total score is " + count + ". Starting a new game.");
+    count = 0;
+    sequence.length = 0;
+    sequence.push(getRandomPanel());
+    sequenceToGuess = [...sequence];
+    setTimeout(() => {
+        main();
+    }, 500);
+}
+
+const panelClicked = (panelClicked) => {
+    if (!canClick) return;
+
+    const expectedPanel = sequenceToGuess.shift();
+
+    if (expectedPanel === panelClicked) {
+        count += 1;
+        playSound();
+        if (sequenceToGuess.length === 0) {
             sequence.push(getRandomPanel());
-            sequenceToGuess=[...sequence];
-            setTimeout(()=>{
+            sequenceToGuess = [...sequence];
+            canClick = false;
+            setTimeout(() => {
                 main();
-            },500)
+            }, 500);
         }
-    }
-    else{
-        alert("Your total score is "+ count);
+    } else {
+        resetGame();
     }
 }
 
-// const resetGame =() => {
-//     count=0;
-//     canClick=false;    
-//     sequence=[getRandomPanel()];
-//     sequenceToGuess=[...sequence];
-//     setTimeout(()=>{
-//         main();
-//     },500);
-// }
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        panelClicked(panel);
+    });
+});
 
-const main= async() => {
-    // canClick=false;
-    for(const panel of sequence){
-        await flash(panel)
+const main = async () => {
+    canClick = false;
+    for (const panel of sequence) {
+        await flash(panel);
     }
-    // canClick=true;
+    canClick = true;
 }
+
+main();
